955a49f08b2b24b03a10c885412aa946eec62245
Jashanpreet Singh <jashan.pahwa007@gmail.com>
2018-03-17 18:24:11 -0400

    fix issues

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -10,10 +10,14 @@ public class Greeter {
    */
   public Greeter() {
 
-  }
 
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+
+  /**
+   * @Param someone the name of the person
+   * @retrn the greeting string
+   */
+
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
